b40ec35d5b4cfdc2233d5c4960fab6fb847870c1
Edgar Martinez <admin@orchid1software.com>
2013-04-03 21:04:04 -0700

    Added comments

--- js/jquery.vplus.js
+++ js/jquery.vplus.js
@@ -1,10 +1,29 @@
+/*
+ vPlus a jQuery form validation plugin
+ Copyright (C) 2013  Edgar Martinez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//todos: Add validation for radio buttons
+//todos: Add validation for select boxes
+//todos: Add validation for textareas
+//todos: Minify the plugin (set up a build process with grunt)
+//todos: Add unit tests
+
 (function($) {
   var setError;
-
-  //todos: Add validation for radio buttons
-  //todos: Add validation for select boxes
-  //todos: Add validation for textareas
-  //todos: Add unit tests
   var methods = {
     vpRequired: function(expected) {
       return this.val();
